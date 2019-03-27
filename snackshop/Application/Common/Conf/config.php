08fd4a02b9fa60ef64ed4c6040b8dd2d55651cda
suexiaopeng <384282882@qq.com>
2019-03-27 11:21:12 +0800

    数据库配置修改

--- snackshop/Application/Common/Conf/config.php
+++ snackshop/Application/Common/Conf/config.php
@@ -5,7 +5,7 @@ return array(
     'DB_TYPE'               =>  'mysql',       // 数据库类型
     'DB_HOST'               =>  '127.0.0.1',   // 服务器地址
     'DB_NAME'               =>  'shipin',          // 数据库名
-    'DB_USER'               =>  'root',       // 用户名
+    'DB_USER'               =>  'sue',       // 用户名
     'DB_PWD'                =>  'swp0601',       // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  't_',         // 数据库表前缀
