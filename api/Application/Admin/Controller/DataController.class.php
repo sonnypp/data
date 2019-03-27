7f6e7aeffe71de8917be31294277a5775c4099ce
suexiaopeng <384282882@qq.com>
2019-03-27 19:39:35 +0800

    零食商城数据统计接口与页面

--- api/Application/Admin/Controller/DataController.class.php
+++ api/Application/Admin/Controller/DataController.class.php
@@ -15,26 +15,57 @@ class DataController extends Controller
 {
     public function sales()
     {
+        //销售额数据统计
         $sales = M("sales");
         $catelog = M("catelog");
         $map['catelog_del'] = 'no';
         $cate = $catelog->where($map)->select();
-        var_dump($cate);
-        exit;
+        $lengend = array();
+        foreach ($cate as $k => $v) {
+            array_push($lengend,$v["catelog_name"]);
+        }
         $data = $sales->select();
+        $series = array();
+        foreach ($cate as $k =>$v) {
+            $series[$k] = array();
+            foreach ($data as $k1 => $v1) {
+                if($v["catelog_id"] == $v1["data_catelog_id"]) {
+                    $series[$k][$v1["data_week"]] = $v1["data_total"];
+                }
+            }
+        }
         $res["code"] = 0;
         $res["msg"] = "";
-        $res["data"] = $data;
+        $res["lengend"] = $lengend;
+        $res["data"] = $series;
         echo $this->ajaxReturn($res,'json');
     }
 
     public function salesvolume()
     {
-        $sales = M("salesvolume");
-        $data = $sales->select();
+        //销售量数据统计
+        $salesvolume = M("salesvolume");
+        $catelog = M("catelog");
+        $map['catelog_del'] = 'no';
+        $cate = $catelog->where($map)->select();
+        $lengend = array();
+        foreach ($cate as $k => $v) {
+            array_push($lengend,$v["catelog_name"]);
+        }
+        $data = $salesvolume->select();
+        $series = array();
+        foreach ($cate as $k =>$v) {
+            $series[$k] = array();
+            foreach ($data as $k1 => $v1) {
+                if($v["catelog_id"] == $v1["data_catelog_id"]) {
+                    $series[$k][$v1["data_week"]] = $v1["data_num"];
+                }
+            }
+        }
         $res["code"] = 0;
         $res["msg"] = "";
-        $res["data"] = $data;
+        $res["lengend"] = $lengend;
+        $res["data"] = $series;
         echo $this->ajaxReturn($res,'json');
     }
 }
